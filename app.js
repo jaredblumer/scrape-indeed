c384bad37bf55d55fef712b029177699a2e7a97b
Jared Blumer <jaredgblumer@gmail.com>
2018-09-26 21:57:50 -0400

    Add promises to asynchronous functions

--- app.js
+++ app.js
@@ -15,66 +15,105 @@ var results = {};
 var links = [];
 var countArr = [];
 var jobCount;
-var startNum = 0;
 
-// Collect all job links
-request(url, function(error, response, body) {
-  var $ = cheerio.load(body);
-  // Parse number of jobs
-  countArr = $('#searchCount').text().trim().split(' ');
-  jobCount = countArr[3];
-  // Collect links
-  $('a[data-tn-element="jobTitle"]').each(function(i, elem) {
-    links.push(base + elem.attribs.href);
-  });
-  // Decrement remaining jobs and request new pages
-  jobCount -= 50;
-  while(jobCount > 0) {
-    url = 'https://www.indeed.com/jobs?q=javascript&l=Pittsburgh%2C+PA&limit=50&radius=25';
-    startNum += 50;
-    url += '&start=' + startNum;
-    collectLinks(url);
-    jobCount -= 50;
-  }
-});
 
-function collectLinks(url) {
-  request(url, function(error, response, body) {
-    var $ = cheerio.load(body);
-    $('a[data-tn-element="jobTitle"]').each(function(i, elem) {
-      links.push(base + elem.attribs.href);
+// Collect all search result pages and return as array
+function initialize() {
+  return new Promise(function(resolve, reject) {
+    request(url, function(error, response, body) {
+      if (error) {
+          reject(error);
+      } else {
+        var $ = cheerio.load(body);
+        // Parse number of jobs
+        countArr = $('#searchCount').text().trim().split(' ');
+        jobCount = countArr[3];
+        console.log("Total Number of Links:", jobCount);
+        // Generate Indeed search result pages in resulting array
+        var indeedPages = [];
+        var startNum = 0;
+        while(jobCount > 0) {
+          var url = 'https://www.indeed.com/jobs?q=javascript&l=Pittsburgh%2C+PA&limit=50&radius=25';
+          url += '&start=' + startNum;
+          indeedPages.push(url);
+          startNum += 50;
+          jobCount -= 50;
+        }
+        console.log("Indeed Search Result Pages:", indeedPages);
+        resolve(indeedPages);
+      }
     });
+  });
+}
+
+var errHandler = function(error) {
+  console.error(error);
+};
+
+function requestPage(url) {
+  return new Promise(function(resolve, reject) {
+    request(url, function(error, response, body) {
+      if (error) {
+        reject(error);
+      } else {
+
+        var $ = cheerio.load(body);
+        $('a[data-tn-element="jobTitle"]').each(function(i, elem) {
+          links.push(base + elem.attribs.href);
+        });
+        console.log("Links Collected:", links.length);
+
+        return resolve(links);
+      }
+    });
+  });
+}
+
+function allPages(indeedPages) {
+  var promises = [];
+  indeedPages.forEach(function(item) {
+    promises.push(requestPage(item));
+  });
+  Promise.all(promises).then(function() {
     console.log(links);
-  })
-}
-
-// Parse job description and call search function
-function parse(parseURL) {
-  var jobDescription = "";
-  request(parseURL, function(error, response, body) {
-    var $ = cheerio.load(body);
-    var text = $('.jobsearch-JobComponent-description').contents().each(
-      function(i, elem) {
-        jobDescription += $(this).text();
-      }
-    );
-    searchForSkills(jobDescription);
   });
 }
 
-// Iterate through skills array and add found skills to results object
-function searchForSkills(jobDescription) {
-  // console.log(jobDescription);
-  skills.skillsArr.forEach(function(element) {
-    var expr = new RegExp(element, 'i');
-    if(expr.test(jobDescription)) {
-      if(results[element]) {
-        results[element] += 1;
-      }
-      else {
-        results[element] = 1;
-      }
-    }
-  });
-  console.log(results);
+function main() {
+ var initalizePromise = initialize().then(allPages, errHandler);
+
 }
+
+main();
+
+//
+// // Parse job description and call search function
+// function parse(parseURL) {
+//   var jobDescription = "";
+//   request(parseURL, function(error, response, body) {
+//     var $ = cheerio.load(body);
+//     var text = $('.jobsearch-JobComponent-description').contents().each(
+//       function(i, elem) {
+//         jobDescription += $(this).text();
+//       }
+//     );
+//     searchForSkills(jobDescription);
+//   });
+// }
+//
+// // Iterate through skills array and add found skills to results object
+// function searchForSkills(jobDescription) {
+//   // console.log(jobDescription);
+//   skills.skillsArr.forEach(function(element) {
+//     var expr = new RegExp(element, 'i');
+//     if(expr.test(jobDescription)) {
+//       if(results[element]) {
+//         results[element] += 1;
+//       }
+//       else {
+//         results[element] = 1;
+//       }
+//     }
+//   });
+//   console.log(results);
+// }
